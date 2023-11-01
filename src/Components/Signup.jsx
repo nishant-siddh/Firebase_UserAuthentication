030f0130684ff3345dada88e5fc3e595c95f56b2
Nishant Siddh <94243692+nishant-siddh@users.noreply.github.com>
2023-11-01 18:45:51 +0530

    Update Signup.jsx

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import InputControl from './inputControl'
+import InputControl from './InputControl'
 import { signup } from '../Firebase/Firebase'
 import { updateProfile } from 'firebase/auth'
 import { useHomeContext } from '../ContextAPI/Context/HomeContext'
